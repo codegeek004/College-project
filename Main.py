8602de6f323880a0a52d8b078c74637c1764eb2d
Yash Vaishnav <yashvaishnav1404@gmail.com>
2024-03-02 20:41:25 +0530

    Initialized application

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-import random
+import argparse
+import os
+import OCR 
+import Linguist
+
+def main():
+    """
+    Handles command line arguments and begins the real-time OCR by calling ocr_    stream().
+    A path to the Tesseract cmd root is required, but all other params are opti    onal.
+
+    Example command-line use: python3 Main.py -t /usr/local/Cellar/tesseract/4.    1.1/bin/tesseract
+    """
+
