ceeee53d21ec821c77ec2a332b3702f822f576be
Yash Vaishnav <yashvaishnav1404@gmail.com>
2024-03-02 21:14:09 +0530

    Lazy engineer

--- Main.py
+++ Main.py
@@ -5,9 +5,7 @@ import Linguist
 
 def main():
     """
-    Handles command line arguments and begins the real-time OCR by calling ocr_    stream().
-    A path to the Tesseract cmd root is required, but all other params are opti    onal.
+    Handles command line arguments and begins the real-time OCR by calling ocr_stream().A path to the Tesseract cmd root is required, but all other params areoptional.
 
     Example command-line use: python3 Main.py -t /usr/local/Cellar/tesseract/4.    1.1/bin/tesseract
-    """
-
+    ""
