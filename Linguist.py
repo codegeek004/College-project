8b2710e5da20e3c1567cfcecf9e8a6c233e8ba81
Yash Vaishnav <yashvaishnav1404@gmail.com>
2024-03-04 14:28:23 +0530

    Intial commit

--- Linguist.py
+++ Linguist.py
@@ -11,4 +11,41 @@ def supported_langs_file():
 def get_language_from_code(code):
     file = supported_langs_file()   
     l_dict = {}
+    with open(file,'r') as f:
+        reader = csv.reader(file,delimiter='\t')
+        for el in reader:
+            key = el[0]
+            name = el[1]
+            l_dict[key] = name  
+
+    try:
+        return l_dict[code]
+    except KeyError:
+        return code
+
+
+def show_codes():
+    """Print a list of all tesseract-supported language codes next to the full language name"""
+
+    file = supported_langs_file()
+
+    with open(file,'r') as f:
+        reader = csv.reader(file,delimiter='\t')
+        print("{:<20s}{:<40s}".format("CODE","LANGUAGE"))
+        for el in reader:
+            print("{:<20s}{:<40s}".format(el[0],el[1]))
+
+def language_string(language):
+    """Generate a string containing a full language name given its code as used in the OCR process"""
+
+    if language is None:
+        name_list = []
+        codes = language.split('+')
+        name = get_language_from_code(language)
+        name_list.append(name)
+        lang_name = ', '.join(name_list)
+    else:
+        lang_name = 'English'
+    return lang_name
+
         
